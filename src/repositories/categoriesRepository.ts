137a90d107b42b4a3755156231a9d9efbed5e27b
Olena Babii <gracetomlinson26@gmail.com>
2023-08-07 02:25:17 +0300

    categories repository updates

--- src/repositories/categoriesRepository.ts
+++ src/repositories/categoriesRepository.ts
@@ -1,15 +1,9 @@
-import { MongoClient} from "mongodb";
 import { Category } from "../helpers/Category";
-
-const MONGO_URI = "mongodb://localhost:27017";
-const DB_NAME = "mydb";
-
-const client = new MongoClient(MONGO_URI);
-const database = client.db(DB_NAME);
-
-const categories = database.collection<Category>("categories");
+import { getDatabase } from "../helpers/db";
 
 export async function findAllCategories() {
+  const database = getDatabase();
+  const categories = database.collection<Category>("categories");
   const allCategories: Category[] = await categories.find({}).toArray();
   return allCategories;
 }
